fdd7f7b27d3bbe2d0e8b466768d7997446097c02
ken <ken10t3@gmail.com>
2020-05-11 23:27:10 +0700

    First commit

---
+++ app/src/main/java/ken/maker/com/testfirebase/MainActivity.java
@@ -0,0 +1,91 @@
+package ken.maker.com.testfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    Button xBDNhietDo, xBDDoAm;
+    TextView tv_nhietDoC, tvNhietDoF;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        xBDNhietDo = findViewById(R.id.btn_bd_nhietdo);
+        xBDDoAm = findViewById(R.id.btn_bd_doam);
+        tv_nhietDoC = findViewById(R.id.tv_nhietdo_c);
+        tvNhietDoF = findViewById(R.id.tv_nhietdo_f);
+
+        xBDNhietDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NhietDoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        readData();
+        loopData();
+
+    }
+
+    private void loopData() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (true) {
+                    try {
+                        Thread.sleep(2000L);
+
+                        readData();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread.start();
+
+    }
+
+    private void readData() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Device/EsptoMobile/nhietdo");
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String key  = ds.getKey();
+                    if (key.contains("C")) {
+                        tv_nhietDoC.setText(  ds.getValue(Integer.class)+ " *C");
+                    }
+                    if (key.contains("F")) {
+                        tvNhietDoF.setText(  ds.getValue(Integer.class)+ " * F");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        databaseReference.addValueEventListener(eventListener);
+    }
+
+    private void controlLed() {
+
+    }
+}
---
+++ app/src/main/java/ken/maker/com/testfirebase/NhietDoActivity.java
@@ -0,0 +1,132 @@
+package ken.maker.com.testfirebase;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NhietDoActivity extends AppCompatActivity {
+
+    List<DataEntry> seriesData;
+    Cartesian cartesian;
+    AnyChartView anyChartView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nhietdo);
+        seriesData = new ArrayList<>();
+
+        getListData();
+
+        anyChartView = findViewById(R.id.any_chart_view);
+
+        cartesian = AnyChart.line();
+
+        cartesian.animation(true);
+
+        cartesian.padding(10d, 20d, 5d, 20d);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("Biểu đồ nhiệt độ.");
+
+        cartesian.yAxis(0).title("Đơn vị Độ C");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+
+
+
+    }
+
+    private class CustomDataEntry extends ValueDataEntry {
+
+        CustomDataEntry(String x, Number value) {
+            super(x, value);
+        }
+    }
+
+    private void getListData() {
+        DatabaseReference mDatabase;
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("test/update/nhietdobd");
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("Data", "ker : " + dataSnapshot.getKey() + "Value : = " + dataSnapshot.getValue());
+                seriesData.add(new CustomDataEntry(dataSnapshot.getKey(),Double.valueOf(String.valueOf(dataSnapshot.getValue()))));
+                Set set = Set.instantiate();
+                set.data(seriesData);
+                Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+
+                Line series1 = cartesian.line(series1Mapping);
+                series1.name("Nhiệt độ");
+                series1.hovered().markers().enabled(true);
+                series1.hovered().markers()
+                        .type(MarkerType.CIRCLE)
+                        .size(4d);
+                series1.tooltip()
+                        .position("right")
+                        .anchor(Anchor.LEFT_CENTER)
+                        .offsetX(5d)
+                        .offsetY(5d);
+
+                cartesian.legend().enabled(true);
+                cartesian.legend().fontSize(13d);
+                cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+                anyChartView.setChart(cartesian);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+
+        };
+
+        mDatabase.addChildEventListener(childEventListener);
+
+
+    }
+
+}
